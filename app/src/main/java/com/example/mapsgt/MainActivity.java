09619f55fa67813800fb7005fe67aa59fc77e289
DuyAlann <datnguyen180502@gmail.com>
2023-03-20 17:22:42 +0700

    navigation MapsGT

--- app/src/main/java/com/example/mapsgt/MainActivity.java
+++ app/src/main/java/com/example/mapsgt/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.mapsgt;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 
 import com.example.mapsgt.map.MapsFragment;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static final int FRAGMENT_HOME = 0;
+    private static final int FRAGMENT_FAVORITE = 1;
+    private static final int FRAGMENT_HISTORY = 2;
+
+    private int mCurrentFragment = FRAGMENT_HOME;
+    private DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +32,58 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.main_holder_map, mapsFragment)
                 .commit();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        replaceFragment(new MapsFragment());
+        navigationView.getMenu().findItem(R.id.nav_home).setChecked(true);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_home){
+            if (mCurrentFragment != FRAGMENT_HOME){
+                replaceFragment(new MapsFragment());
+                mCurrentFragment = FRAGMENT_HOME;
+            }
+        } else if (id == R.id.nav_favorite){
+            if (mCurrentFragment != FRAGMENT_FAVORITE){
+                replaceFragment(new MapsFragment());
+                mCurrentFragment = FRAGMENT_FAVORITE;
+            }
+        } else if (id == R.id.nav_history){
+            if (mCurrentFragment != FRAGMENT_HISTORY){
+                replaceFragment(new MapsFragment());
+                mCurrentFragment = FRAGMENT_HISTORY;
+            }
+        }
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)){
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_holder_map, fragment);
+        transaction.commit();
     }
 }
