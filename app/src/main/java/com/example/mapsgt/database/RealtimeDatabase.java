d26388b4162c9a1f1cd2fbb2628c9f5f5d658d36
Ducan Nguyen <ducan172002@gmail.com>
2023-04-09 16:24:52 +0700

    realtime database

---
+++ app/src/main/java/com/example/mapsgt/database/RealtimeDatabase.java
@@ -0,0 +1,30 @@
+package com.example.mapsgt.database;
+
+import androidx.lifecycle.LiveData;
+
+import com.example.mapsgt.data.entities.User;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+
+public abstract class RealtimeDatabase<T> {
+    private final DatabaseReference databaseReference;
+
+    public RealtimeDatabase(DatabaseReference databaseReference) {
+        this.databaseReference = databaseReference;
+    }
+
+    public DatabaseReference getDatabaseReference() {
+        return databaseReference;
+    }
+
+    public abstract void insert(T object);
+
+    public abstract void update(T object);
+
+    public abstract void delete(T object);
+
+    public abstract void deleteAll();
+
+    public abstract LiveData<ArrayList<User>> getAll();
+}
