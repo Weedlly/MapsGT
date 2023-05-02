4b5ee94d50271a879e0e3ca4fd1afbd88115bd9a
starhuy1903 <huykst137@gmail.com>
2023-05-02 09:38:50 +0700

    feat: create user profile page

---
+++ app/src/main/java/com/example/mapsgt/ui/user_profile/UserProfileActivity.java
@@ -0,0 +1,98 @@
+package com.example.mapsgt.ui.user_profile;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.mapsgt.R;
+import com.example.mapsgt.data.entities.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class UserProfileActivity extends AppCompatActivity {
+    private ImageView avatarImg;
+    private TextView nameTv;
+    private TextView friendNumberTv;
+    private TextView emailTv;
+    private TextView phoneNumberTv;
+    private TextView locationTv;
+    private Button editBtn;
+
+    private DatabaseReference mDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        avatarImg = findViewById(R.id.iv_avatar);
+        nameTv = findViewById(R.id.tv_name);
+        friendNumberTv = findViewById(R.id.tv_friend_number);
+        emailTv = findViewById(R.id.tv_email);
+        phoneNumberTv = findViewById(R.id.tv_phone_number);
+        locationTv = findViewById(R.id.tv_location);
+        editBtn = findViewById(R.id.btn_edit_profile);
+
+        editBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        getUserInfo();
+    }
+
+    private void getUserInfo() {
+        String currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User response = snapshot.child("users").child(currentUserId).getValue(User.class);
+                if (response != null) {
+                    String personProfileImage = response.getProfilePicture();
+
+                    RequestOptions options = new RequestOptions()
+                            .placeholder(R.drawable.ic_profile)
+                            .error(R.drawable.google);
+
+                    Glide.with(UserProfileActivity.this)
+                            .load(personProfileImage)
+                            .apply(options)
+                            .into(avatarImg);
+
+                    nameTv.setText(response.getFirstName() + " " + response.getLastName());
+
+                    emailTv.setText(response.getEmail());
+                    phoneNumberTv.setText(response.getPhone());
+                    locationTv.setText("");
+                    DataSnapshot friendsRef = snapshot.child("users").child(currentUserId).child("friends");
+                    if (friendsRef.hasChildren()) {
+                        friendNumberTv.setText(String.valueOf(friendsRef.getChildrenCount()));
+                    } else {
+                        friendNumberTv.setText(0);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
